f93586cf42bc071d5f69e24fc94ee8b9a2e19881
Christopher Lamm <chris@theantichris.com>
2013-02-07 14:53:24 -0500

    Built out basic HTML, <head>, and included WordPress shipped version of jQuery.

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
 
 /**
- * Created by JetBrains PhpStorm.
- * User: chris.lamm
- * Date: 2/7/13
- * Time: 10:15 AM
- * To change this template use File | Settings | File Templates.
+ * Home page and default template.
+ *
+ * @since 1.00
  */
+
+// TODO: Change wptbp_ prefix
+
 ?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
+
+	<title><?php wptbp_get_title(); ?></title>
+
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen"/>
+
+	<?php wp_head(); ?>
+</head>
+
+<?php wp_enqueue_script( 'jquery' ); ?>
+</html>
