204da639cb67939a0cbfa7cf6d63aa059a56ecc2
Christopher Lamm <chris@theantichris.com>
2013-02-14 14:09:18 -0500

    Archive page content.

--- archive.php
+++ archive.php
@@ -8,3 +8,45 @@
  */
 
 ?>
+
+<?php get_header(); ?>
+
+<?php get_sidebar(); ?>
+
+<div id="main-content">
+
+	<?php if ( have_posts() ) : ?>
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+			<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+			<?php echo get_post_meta( $post->ID, 'PostThumb', true ); ?>
+
+			<p class="meta">
+				<span>Posted on</span> <?php the_time( 'F jS, Y' ); ?> <span>by</span> <?php the_author(); ?>
+			</p>
+
+			<?php the_content( 'Read Full Article' ); ?>
+
+			<p>
+				<?php the_tags( 'Tags: ', ', ', '<br />' ); ?>
+				Posted in <?php the_category( ', ' ); ?>
+				<?php comments_popup_link( 'No Comments', '1 Comment', '% Comments' ); ?>
+			</p>
+		</div>
+
+		<?php endwhile; ?>
+
+	<?php else: ?>
+
+	<h2>Nothing Found</h2>
+
+	<?php endif; ?>
+
+</div>
+
+<?php get_sidebar( 'secondary' ); ?>
+
+<?php get_footer(); ?>
