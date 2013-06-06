c84d38cf5e8a3c3859448b4b1fa0f025cd809afc
theantichris <chris@theantichris.com>
2013-06-06 16:42:49 -0400

    Added container div to content.

--- index.php
+++ index.php
@@ -20,7 +20,9 @@ get_header();
 			<article id="post-<?php the_ID(); ?>" <?php post_class() ?>>
 				<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
-				<?php the_content(); ?>
+				<div>
+					<?php the_content(); ?>
+				</div>
 			</article>
 
 		<?php endwhile; ?>
