c0e9c4e1bda474012bee81bd704789635885f33b
theantichris <chris@theantichris.com>
2013-06-06 16:41:01 -0400

    Moved post class to after post ID.

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ get_header();
 
 		<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
 
-			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<article id="post-<?php the_ID(); ?>" <?php post_class() ?>>
 				<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
 				<?php the_content(); ?>
