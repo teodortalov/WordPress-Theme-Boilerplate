4bab6fc9bf4615d11be745a0be0719ef479952e6
theantichris <chris@theantichris.com>
2013-06-06 16:57:44 -0400

    Updated 404.php.

--- 404.php
+++ 404.php
@@ -3,7 +3,24 @@
 /**
  * Error page, served up when a URL is used that does not exist.
  *
- * @package WordPressThemeBoilerplate
- * @author Christopher Lamm <chris@theantichris.com>
- * @since 1.0.0
+ * @package    WordPress
+ * @subpackage WordPressThemeBoilerplate
+ * @author     Christopher Lamm <chris@theantichris.com>
+ * @since      1.0.0
  */
+
+get_header()
+
+?>
+
+	<div id="content" role="main">
+		<h2><?php _e( 'Error 404 - Page Not Found', 'wptbp' ); ?></h2>
+	</div>
+
+<?php
+
+get_sidebar();
+
+get_footer();
+
+?>
