26294fa6116bbe37c499688afcde40b2b3816a90
theantichris <chris@theantichris.com>
2013-06-06 16:56:03 -0400

    Cleaned up post titles.

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ get_header();
 		<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class() ?>>
-				<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+				<h2><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 
 				<div>
 					<?php the_content(); ?>
--- single.php
+++ single.php
@@ -19,7 +19,7 @@ get_header();
 		<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class() ?>>
-				<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+				<h1><?php the_title(); ?></h1>
 
 				<div>
 					<?php the_content(); ?>
