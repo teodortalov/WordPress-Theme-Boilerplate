97740243ba8da4bc62f5f682b6dd0423c31e1327
theantichris <chris@theantichris.com>
2013-06-06 15:51:25 -0400

    Update header.php and added iOS icon stuff.

--- header.php
+++ header.php
@@ -1,11 +1,12 @@
 <?php
 
 /**
- * Included at the top of every page.
+ * Included at the top of every page. Includes the doctype, <head> tag, opening <body> tag, <header> tag, and <nav> tag.
  *
- * @package WordPressThemeBoilerplate
- * @author Christopher Lamm <chris@theantichris.com>
- * @since 1.0.0
+ * @package    WordPress
+ * @subpackage WordPressThemeBoilerplate
+ * @author     Christopher Lamm <chris@theantichris.com>
+ * @since      1.0.0
  */
 
 ?>
@@ -13,17 +14,35 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 
-	<title><?php echo wptbp_get_title(); // TODO: Change function prefix ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<meta name="description" content="<?php bloginfo( 'description' ); ?>" />
+
+	<meta name="author" content="Christopher Lamm" /> <?php // TODO: Change author name. ?>
+
+	<meta name="Copyright" content="Copyright &copy; <?php bloginfo( 'name' ); ?> <?php echo date( 'Y' ); ?>. All Rights Reserved.">
+
+	<title><?php echo wptbp_get_title(); // TODO: Change function prefix. ?></title>
 
 	<?php wp_head(); ?>
+
+	<link rel="shortcut icon" href="<?php bloginfo( 'stylesheet_directory' ); ?>/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo( 'stylesheet_directory' ); ?>/apple-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo( 'stylesheet_directory' ); ?>/apple-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo( 'stylesheet_directory' ); ?>/apple-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo( 'stylesheet_directory' ); ?>/apple-icon-144x144.png" />
 
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen, projection" />
 </head>
 
 <body <?php body_class(); ?>>
 
-<header>
-	<?php wp_nav_menu( 'primary' ); // Place primary navigation menu location ?>
+<header id="header" role="header">
+
 </header>
+
+<nav id="nav" role="navigation">
+
+</nav>
